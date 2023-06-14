56de4c61e58447168f4006eb432f6ba2f049a4e4
DanielFalconGuerra <DanielFalcon.87@gmail.com>
2023-06-14 09:59:23 -0600

    getChampionshipByTeam

--- routes/index.js
+++ routes/index.js
@@ -33,9 +33,34 @@ const getEquipos = (request, response) => {
     });
 };
 
+const GetPlayersByTeam = (request, response) => {
+    var IdEquipo = request.params.idequipo;
+    connection.query(`select j.IdJugador, j.Nombre, j.Edad, j.Dorsal, p.Nombre as Pais, pos.Abreviacion, pos.Nombre as Posicion  from (select * from jugador where IdEquipo = ${IdEquipo})j
+                                            join pais p on p.IdPais = j.IdPais 
+                                            join posicion pos on pos.IdPosicion = j.IdPosicion`, 
+    (error, results) => {
+        if(error)
+            throw error;
+        response.status(200).json(results);
+    });
+};
+
+const GetTeamChampionships = (request, response) => {
+    var IdEquipo = request.params.idequipo;
+    connection.query(`select c.IdCampeonato, t.Nombre, c.cantidad from (select * from campeonato where IdEquipo = ${IdEquipo}) c 
+                        join titulo t on t.IdTitulo = c.IdTitulo`, 
+    (error, results) => {
+        if(error)
+            throw error;
+        response.status(200).json(results);
+    });
+};
+
 //ruta
 app.route("/ligas").get(getLiga);
+app.route("/jugadores/:idequipo").get(GetPlayersByTeam);
 app.route("/equipos/:idliga").get(getEquipos);
+app.route("/equipos/titulos/:idequipo").get(GetTeamChampionships);
 
 
 module.exports = app;
